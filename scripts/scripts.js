15a362d401b022b197aafed29bc60f9eb49d476f
lhebia <lhebia@gmail.com>
2020-06-06 18:34:57 -0400

    Minor commenting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -168,6 +168,9 @@ noahsApp.setWinCounter = function() {
 }
 
 noahsApp.checkForMatch = function(clickedButton) {
+    console.log(clickedButton);
+    console.log(this);
+    console.log($(this));
     const clickedButtonInner = clickedButton[0].innerHTML;
     if (noahsApp.clickedCard) {
         if (clickedButtonInner === noahsApp.clickedCard[0].innerHTML) {
